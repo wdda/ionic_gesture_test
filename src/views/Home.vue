81f520e7eb3d2b450a92f50e06b5febf30cd40e2
WDDA <dima@wdda.pro>
2021-11-30 13:17:30 +0300

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
       <div id="container">
         <div>Info one: {{ this.infoOne }}</div>
         <div>Info two: {{ this.infoTwo }}</div>
